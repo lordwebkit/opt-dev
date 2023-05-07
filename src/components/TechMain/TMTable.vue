4bff3a8b0f93a45a56044ea0a2dd3b704be63376
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-05-07 16:11:40 +0300

    fix a bug with dragging only the names of the tables

--- src/components/TechMain/TMTable.vue
+++ src/components/TechMain/TMTable.vue
@@ -110,13 +110,24 @@ function startDragAndDrop(event, index) {
     startCursorPosition.value = event.clientX
 
     let draggingColumn = document.querySelectorAll('.tm-table__th')[index]
+    let draggingData = document.querySelectorAll(
+      `.tm-table__td[data-name=${draggingColumn.dataset.field}]`
+    )
     draggingColumnPosition.value = draggingColumn.getBoundingClientRect().x
     draggingColumn.style.position = 'absolute'
     draggingColumn.style.zIndex = 1000
+    draggingData.forEach((data) => {
+      data.style.position = 'absolute'
+      data.style.zIndex = 1000
+    })
 
-    let nextColumn = document.querySelectorAll('.tm-table__th')[index + 1]
-    if (nextColumn) {
+    if (draggingColumnIndex.value < 6) {
+      let nextColumn = document.querySelectorAll('.tm-table__th')[index + 1]
+      let nextData = document.querySelectorAll(`.tm-table__td[data-name=${nextColumn.dataset.field}]`)
       nextColumn.style.marginLeft = 100 + 'px'
+      nextData.forEach((data) => {
+        data.style.marginLeft = 100 + 'px'
+      })
     }
 
     document.addEventListener('mousemove', handleDragAndDrop)
@@ -127,19 +138,28 @@ function startDragAndDrop(event, index) {
 function handleDragAndDrop(event) {
   if (resizingColumnIndex.value === null) {
     let draggingColumn = document.querySelectorAll('.tm-table__th')[draggingColumnIndex.value]
+    let draggingData = document.querySelectorAll(
+      `.tm-table__td[data-name=${draggingColumn.dataset.field}]`
+    )
     let tablePosition = document.querySelector('.tm-table').getBoundingClientRect().x
     let cursorPosition = event.clientX - (startCursorPosition.value - draggingColumnPosition.value)
 
     if (tablePosition < cursorPosition && tablePosition + tableWidth.value - 100 > cursorPosition) {
       draggingColumn.style.left = cursorPosition + 'px'
+      draggingData.forEach((data) => {
+        data.style.left = cursorPosition + 'px'
+      })
     }
   }
 }
 
 function stopDragAndDrop(event) {
   let draggingColumn = document.querySelectorAll('.tm-table__th')[draggingColumnIndex.value]
-  let nextColumn = document.querySelectorAll('.tm-table__th')[draggingColumnIndex.value + 1]
-  let stopCursorPosition = event.clientX - (startCursorPosition.value - draggingColumnPosition.value)
+  let draggingData = document.querySelectorAll(
+    `.tm-table__td[data-name=${draggingColumn.dataset.field}]`
+  )
+  let stopCursorPosition =
+    event.clientX - (startCursorPosition.value - draggingColumnPosition.value)
 
   if (stopCursorPosition - draggingColumnPosition.value > 51) {
     let draggingColumnOnIndex = Math.ceil((stopCursorPosition - draggingColumnPosition.value) / 100)
@@ -148,15 +168,27 @@ function stopDragAndDrop(event) {
     swapColumns(replacingColumnIndex)
   } else if (stopCursorPosition - draggingColumnPosition.value < -51) {
     let draggingColumnOnIndex = Math.ceil((draggingColumnPosition.value - stopCursorPosition) / 100)
+    if (draggingColumnOnIndex > 6) draggingColumnOnIndex = 6
     let replacingColumnIndex = draggingColumnIndex.value - draggingColumnOnIndex
-    if (replacingColumnIndex > 6) replacingColumnIndex = 6
     swapColumns(replacingColumnIndex)
   }
 
-  if (nextColumn) nextColumn.style.marginLeft = ''
+  if (draggingColumnIndex.value < 6) {
+    let nextColumn = document.querySelectorAll('.tm-table__th')[draggingColumnIndex.value + 1]
+    let nextData = document.querySelectorAll(`.tm-table__td[data-name=${nextColumn.dataset.field}]`)
+    nextColumn.style.marginLeft = ''
+    nextData.forEach((data) => {
+      data.style.marginLeft = ''
+    })
+  }
   draggingColumn.style.position = ''
   draggingColumn.style.left = ''
   draggingColumn.style.zIndex = ''
+  draggingData.forEach((data) => {
+    data.style.position = ''
+    data.style.left = ''
+    data.style.zIndex = ''
+  })
   draggingColumnIndex.value = null
   draggingColumnPosition.value = null
   document.removeEventListener('mousemove', handleDragAndDrop)
@@ -168,8 +200,15 @@ function swapColumns(replacingColumnIndex) {
   const temp = columnsCopy[replacingColumnIndex]
   columnsCopy[replacingColumnIndex] = columnsCopy[draggingColumnIndex.value]
   columnsCopy[draggingColumnIndex.value] = temp
+  const rowsCopy = [...rows.value].map((row) => {
+    const obj = {}
+    columnsCopy.forEach((column) => {
+      obj[column.field] = row[column.field]
+    })
+    return obj
+  })
   columns.value = columnsCopy
-  console.log(replacingColumnIndex, draggingColumnIndex.value)
+  rows.value = rowsCopy
 }
 </script>
 
@@ -178,8 +217,8 @@ function swapColumns(replacingColumnIndex) {
     <table class="tm-table" :style="{ width: tableWidth + 'px' }">
       <thead class="tm-table__head">
         <tr>
-          <th class="tm-table__th" v-for="(column, index) in columns" :key="index" :style="{ width: column.width + 'px' }"
-            @mousedown="startDragAndDrop($event, index)">
+          <th class="tm-table__th" :data-field="column.field" v-for="(column, index) in columns" :key="index"
+            :style="{ width: column.width + 'px' }" @mousedown="startDragAndDrop($event, index)">
             {{ column.title }}
             <div class="tm-table__resize-border" @mousedown="startResize($event, index)"></div>
           </th>
@@ -187,7 +226,7 @@ function swapColumns(replacingColumnIndex) {
       </thead>
       <tbody class="tm-table__body" @mousedown="startDrag($event)">
         <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
-          <td class="tm-table__td" v-for="(value, name, index) in row" :key="name" :style="[
+          <td class="tm-table__td" :data-name="name" v-for="(value, name, index) in row" :key="index" :style="[
             { width: columns.find((col) => col.field == name).width + 'px' },
             { zIndex: ++index }
           ]">
